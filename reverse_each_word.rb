bc89de02519c645318bab6b6735d8daf5f382d2c
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-13 00:24:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
     new_array = string.split(" ")
     return_array = []
     new_array.each do |string|
-      return_array.push(string.reverse)
+      return_array << string.reverse
     end
   return return_array
 end
